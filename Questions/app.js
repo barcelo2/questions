97b538ab897263ced8df786dcd268f9554cd7a0d
Vera <58885611+barcelo2@users.noreply.github.com>
2022-03-25 19:26:45 +0100

    Add files via upload

---
+++ Questions/app.js
@@ -0,0 +1,21 @@
+
+
+//choose ¨question¨, loop over them
+
+const questions= document.querySelectorAll('.question')
+
+questions.forEach(function(question){
+    const btn=question.querySelector('.question-btn')
+
+    
+
+    btn.addEventListener('click',function(){
+        questions.forEach(function(item){
+            if(item !==question){
+                item.classList.remove('show-text')
+            }
+        })
+
+        question.classList.toggle('show-text')
+    })
+})
